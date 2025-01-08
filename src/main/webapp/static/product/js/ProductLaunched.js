783d394f27ee0e37075d0a1ae20d47e76e4ef677
yuran <tp6m4b06to@gmail.com>
2025-01-08 11:10:07 +0800

    aside load 加上驗證是否有登入的方法

--- src/main/webapp/static/product/js/ProductLaunched.js
+++ src/main/webapp/static/product/js/ProductLaunched.js
@@ -8,7 +8,33 @@ document.getElementById("upload").addEventListener("change", function () {
 })
 
 $(function () {
-	$(".aside").load("http://localhost:8080/CloudSerenityHotel/aside");
-	
+    $(".aside").load("http://localhost:8080/CloudSerenityHotel/aside", () => {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://localhost:8080/CloudSerenityHotel/user/getUserInfo",
+            success: (resp) => {
+                if (resp.code === 505) {
+                    alert("請先登入!");
+                    window.location.href =
+                        "http://localhost:8080/CloudSerenityHotel/static/user/login.jsp";
+                } else if (resp.code === 404) {
+                    alert("找不到此帳號!");
+                    window.location.href =
+                        "http://localhost:8080/CloudSerenityHotel/static/user/login.jsp";
+                } else {
+                    if (resp.data.userIdentity != "admin") {
+                        alert("你不是管理員，無權使用此介面");
+                        window.location.href =
+                            "http://localhost:8080/CloudSerenityHotel/user/logout";
+                        return;
+                    }
+                    $("#username").text(`歡迎 ${resp.data.userName} 管理員`);
+                    $("#loginUser").append(`<div><a href="http://localhost:8080/CloudSerenityHotel/user/logout">登出</a></div>`);
+                }
+            },
+        });
+    });
+
 });
 
