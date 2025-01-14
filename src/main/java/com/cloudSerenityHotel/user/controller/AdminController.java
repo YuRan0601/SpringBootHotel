661f06270f417b3da56940f0abcb6fdc1ca2f197
林孟穎 <f97022@gmail.com>
2025-01-14 16:33:16 +0800

    補上@CrossOrigin 避免跨域問題 20250114
    補上@CrossOrigin 避免跨域問題

--- src/main/java/com/cloudSerenityHotel/user/controller/AdminController.java
+++ src/main/java/com/cloudSerenityHotel/user/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.cloudSerenityHotel.user.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
--- src/main/java/com/cloudSerenityHotel/user/controller/MemberController.java
+++ src/main/java/com/cloudSerenityHotel/user/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.cloudSerenityHotel.user.model.User;
 import com.cloudSerenityHotel.user.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @Controller
 public class MemberController {
 
