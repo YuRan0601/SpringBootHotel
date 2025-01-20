6e884d0bd84e96e69eba03040bfce11d60b9423b
cyu <qq411011@gmail.com>
2025-01-20 14:52:36 +0800

    測試上傳圖片

--- src/main/java/com/cloudSerenityHotel/attraction_facility/attraction/service/PictureService.java
+++ src/main/java/com/cloudSerenityHotel/attraction_facility/attraction/service/PictureService.java
@@ -1,7 +1,8 @@
 package com.cloudSerenityHotel.attraction_facility.attraction.service;
 
 import com.cloudSerenityHotel.attraction_facility.attraction.model.Picture;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,7 @@ public interface PictureService {
     Picture createPicture(Picture picture);
     void deletePicture(Integer id);
     Picture updatePicture(Integer id, Picture updatedPicture);
+    
+    // 新增的方法
+    Picture saveFile(MultipartFile file) throws IOException;
 }
