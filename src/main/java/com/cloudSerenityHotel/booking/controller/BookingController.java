c9285f6948b7ceb8a18763ab4d8989418a5e81c4
YuRan0601 <tp6m4b06to@gmail.com>
2025-01-26 21:09:21 +0800

    新增訂單 接值測試

--- src/main/java/com/cloudSerenityHotel/booking/controller/BookingController.java
+++ src/main/java/com/cloudSerenityHotel/booking/controller/BookingController.java
@@ -2,9 +2,13 @@ package com.cloudSerenityHotel.booking.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cloudSerenityHotel.booking.model.BookingOrder;
 import com.cloudSerenityHotel.booking.service.BookingService;
+
+import jakarta.websocket.server.PathParam;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +16,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -25,5 +32,12 @@ public class BookingController {
 	@GetMapping("/order")
 	public List<Map<String, Object>> getAllOrders() {
 		return bService.getAllOrders();
+	}
+	
+	@PostMapping("/order/{roomTypeId}")
+	public void insertOrder(@RequestBody BookingOrder order, @PathVariable Integer roomTypeId) {
+		System.out.println(order.getUser().getUserId());
+		System.out.println(order.getTotalPrice());
+		System.out.println(roomTypeId);
 	}
 }
