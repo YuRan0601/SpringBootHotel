c75cb1e88f1d479a2212c241e79a246f952ded38
xinoxo1120 <miaoxolin1120@gmail.com>
2025-02-03 20:57:04 +0800

    租車預約表 和 使用者表

---
+++ src/main/java/com/cloudSerenityHotel/rent/model/CarReservationUpdateStatusRequest.java
@@ -0,0 +1,11 @@
+package com.cloudSerenityHotel.rent.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CarReservationUpdateStatusRequest {
+
+	private String status;
+}
